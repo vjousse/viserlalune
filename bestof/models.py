b815b34b5ea94ce4763cdb1b9a3a1391e2d388a1
Vincent Jousse <vincent@jousse.org>
2013-09-29 14:55:26 +0200

    Add basic schema and migration to bestof

--- bestof/models.py
+++ bestof/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.URLField()
+    description = models.TextField()
