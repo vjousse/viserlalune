b1743f6d4e24c10835dba311265c335919d00aaa
Vincent Jousse <vincent@jousse.org>
2013-09-30 20:48:43 +0200

    Revert "Add basic schema and migration to bestof"
    This reverts commit b815b34b5ea94ce4763cdb1b9a3a1391e2d388a1.

--- bestof/models.py
+++ bestof/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Entry(models.Model):
-    title = models.CharField(max_length=250)
-    url = models.URLField()
-    description = models.TextField()
